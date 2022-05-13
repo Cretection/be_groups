f10275b67f357c11b08f083c989a98c25f7d5f51
Jonathan Starck <info@cretection.it>
2022-05-13 20:06:54 +0200

    [TASK] Code cleaning

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,26 +26,25 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-  'title' => 'Improved backend groups management interface.',
-  'description' => 'This extension provide several new options to restructure large amount of be_groups records.',
-  'category' => 'be',
-  'author_company' => 'Cretection',
-  'author' => 'Jonathan Starck',
-  'author_email' => 'info@cretection.it',
-  'state' => 'beta',
-  'uploadfolder' => false,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 1,
-  'version' => '0.0.4',
-  'constraints' => array(
-      'depends' => array(
-          'php' => '7.2.0-8.2.99',
-          'typo3' => '11.4.0-11.5.99',
-      ),
-      'conflicts' => array(
-      ),
-      'suggests' => array(
-      ),
-  ),
-);
+  $EM_CONF[$_EXTKEY] = [
+    'description' => 'This extension provide several new options to restructure large amount of be_groups records.',
+    'category' => 'be',
+    'author_company' => 'Cretection',
+    'author' => 'Jonathan Starck',
+    'author_email' => 'info@cretection.it',
+    'state' => 'beta',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.0.4',
+    'constraints' => [
+      'depends' => [
+        'php' => '7.2.0-8.2.99',
+        'typo3' => '11.4.0-11.5.99',
+      ],
+      'conflicts' => [
+      ],
+      'suggests' => [
+      ],
+    ],
+  ];
