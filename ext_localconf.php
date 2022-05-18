44040d3dc9bdc22ba0087b43bccae2f01645b5c1
Jonathan Starck <info@cretection.it>
2022-05-18 17:56:02 +0200

    Customization of the branch for documentation

--- ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) die('Access denied.');
-
-$configurationArray = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['be_groups']);
-if (is_array($configurationArray) && array_key_exists('explicitAllow', $configurationArray) && $configurationArray['explicitAllow'] == 1) {
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['explicitADmode'] = 'explicitAllow';
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = Cretection\BeGroups\Service\TceMain\ProcessFieldArray::class;
